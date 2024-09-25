a967ddbb528361195d9e5608efacb72a3aec2302
coffeefrenny <reyesc072@gmail.com>
2024-09-25 09:02:25 +0800

    Create welcome.c

---
+++ welcome.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf ("-----*****-----*****-----*****-----\n");
+
+    printf("Welcome to Naga College Foundation\n");
+
+    printf("I am Christian Paul D. Reyes from College of Computer Studies\n");
+
+    printf("-----*****-----*****-----*****-----\n");
+
+    printf("Press any key to continue....\n");
+    return 0;
+
+}
